c74d45c11ecd78053345b7293dba95eaf92b427f
HamBeomJoon <hbj0802@naver.com>
2025-03-21 15:24:27 +0900

    refactor: OmokBoard 함수 구현

--- src/main/kotlin/omok/domain/OmokBoard.kt
+++ src/main/kotlin/omok/domain/OmokBoard.kt
@@ -3,7 +3,27 @@ package omok.domain
 class OmokBoard(val width: Int = DEFAULT_SIZE, val height: Int = DEFAULT_SIZE) {
     val positions: List<Position> = (1..15).flatMap { x -> (1..15).map { y -> Position(x, y) } }
 
+    fun canPlace(position: Position) {
+        if (findPoint(position.x, position.y)?.stoneState != StoneState.BLANK) {
+            throw IllegalArgumentException(
+                ERROR_STONE_ALREADY_PUT,
+            )
+        }
+    }
 
+    fun findPoint(
+        x: Int,
+        y: Int,
+    ): Position? {
+        return positions.find { it.x == x && it.y == y }
+    }
+
+    fun putStone(
+        position: Position,
+        state: StoneState,
+    ) {
+        findPoint(position.x, position.y)?.changeState(state)
+    }
 
     companion object {
         const val DEFAULT_SIZE: Int = 15
